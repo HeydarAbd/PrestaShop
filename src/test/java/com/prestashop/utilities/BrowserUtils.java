8b6b4a49381c12678e0bc5248d16cde4dc733571
Heydar Abdullayev <heydar.abdullayev@gmail.com>
2019-01-23 15:55:15 -0500

    browser Utils addes

---
+++ src/test/java/com/prestashop/utilities/BrowserUtils.java
@@ -0,0 +1,26 @@
+package com.prestashop.utilities;
+
+public class BrowserUtilities {
+
+
+    public static final boolean ISWINDOWS;
+    public static final boolean ISMAC;
+
+    static {
+        ISWINDOWS = System.getProperty("os.name").toLowerCase().contains("windows");
+        ISMAC = System.getProperty("os.name").toLowerCase().contains("mac");
+    }
+
+
+    public static void wait(int secs) {
+        try {
+            Thread.sleep(1000 * secs);
+        }
+        catch (InterruptedException e) {
+
+        }
+    }
+
+
+
+}
